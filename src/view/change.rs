1428845047e632560ba6bb08036506b90303dbff
Matteo Monti <matteo.monti@epfl.ch>
2021-10-21 17:09:06 +0200

    Derive `Hash` for `Change`

--- src/view/change.rs
+++ src/view/change.rs
@@ -1,8 +1,10 @@
 use serde::{Deserialize, Serialize};
 
+use std::hash::Hash;
+
 use talk::crypto::primitives::sign::PublicKey;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub(crate) enum Change {
     Join(PublicKey),
     Leave(PublicKey),
