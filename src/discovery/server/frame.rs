27d4fc80f64fb383e9127f0de94dfadc9299180d
Manuel <mvidigueira@gmail.com>
2021-10-27 16:22:47 +0200

    Add tests to `Frame`

--- src/discovery/server/frame.rs
+++ src/discovery/server/frame.rs
@@ -61,10 +61,10 @@ impl Frame {
             metadata.extend_from_slice(&self.metadata[from..]);
         }
 
-        let mut lookup = ShiftVec::new(self.base);
+        let mut lookup = ShiftVec::new(base);
         let mut last_tailless = 0;
 
-        for (index, metadata) in self.metadata.iter().enumerate() {
+        for (index, metadata) in metadata.iter().enumerate() {
             if metadata.tailless {
                 while lookup.len() < metadata.destination_height {
                     lookup.push(last_tailless)
@@ -130,12 +130,25 @@ mod tests {
 
     use crate::view::test::InstallGenerator;
 
+    async fn setup(genesis: usize, added_views: usize) -> (Frame, InstallGenerator) {
+        let generator = InstallGenerator::new(genesis + added_views);
+        let genesis = generator.view(genesis).await;
+        let frame = Frame::genesis(genesis);
+
+        (frame, generator)
+    }
+
+    fn check(frame: &Frame, genesis: usize, expected: &[usize]) {
+        println!("");
+        for (index, expected) in expected.into_iter().enumerate() {
+            //print!("{}", frame.lookup[genesis + index]);
+            assert_eq!(frame.lookup[genesis + index], *expected);
+        }
+    }
+
     #[tokio::test]
     async fn develop() {
-        let generator = InstallGenerator::new(50);
-        let genesis = generator.view(10).await;
-
-        let frame = Frame::genesis(genesis);
+        let (frame, generator) = setup(10, 40).await;
 
         let i0 = generator.install(10, 15, [16]).await;
         let f0 = frame.update(i0).await.unwrap();
@@ -160,8 +173,91 @@ mod tests {
             5,
         ];
 
-        for (index, expected) in expected.into_iter().enumerate() {
-            assert_eq!(f5.lookup[10 + index], *expected);
+        check(&f5, 10, expected);
+    }
+
+    #[tokio::test]
+    async fn basic() {
+        let (mut frame, generator) = setup(10, 10).await;
+
+        for i in 10..20 {
+            let install = generator.install(i, i+1, []).await;
+            frame = frame.update(install).await.unwrap();
         }
+
+        let expected = &[
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        ];
+
+        check(&frame, 10, expected);
+    }
+
+    #[tokio::test]
+    async fn no_installs() {
+        let (mut frame, generator) = setup(10, 11).await;
+
+        for i in 10..20 {
+            let install = generator.install(i, i+1, [i+2]).await;
+            frame = frame.update(install).await.unwrap();
+        }
+
+        let expected = &[
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ];
+
+        check(&frame, 10, expected);
+    }
+
+    #[tokio::test]
+    async fn new_installs() {
+        let (mut frame, generator) = setup(10, 11).await;
+
+        for i in 10..20 {
+            let install = generator.install(i, i+1, [i+2]).await;
+            frame = frame.update(install).await.unwrap();
+        }
+
+        let expected = &[
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ];
+
+        check(&frame, 10, expected);
+
+        for i in [15, 17] {
+            let install = generator.install(i-1, i, []).await;
+            frame = frame.update(install).await.unwrap();
+        }
+
+        let expected = &[
+            0, 0, 0, 0, 0, 5, 5, 7, 7, 7
+        ];
+
+        check(&frame, 10, expected);
+    }
+
+    #[tokio::test]
+    async fn shortcut() {
+        let (mut frame, generator) = setup(10, 11).await;
+
+        let i0 = generator.install(10, 11, [12, 13]).await;
+        frame = frame.update(i0).await.unwrap();
+
+        let i1 = generator.install(11, 12, [13]).await;
+        frame = frame.update(i1).await.unwrap();
+
+        let expected = &[
+            0, 0,
+        ];
+
+        check(&frame, 10, expected);
+
+        let i2 = generator.install(10, 12, []).await;
+        frame = frame.update(i2).await.unwrap();
+
+        let expected = &[
+            0, 0,
+        ];
+
+        check(&frame, 10, expected);
     }
 }
