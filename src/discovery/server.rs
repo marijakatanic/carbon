19894be7cf88f7546d141c58cbfca56cf7fc770e
Manuel <mvidigueira@gmail.com>
2021-11-01 19:50:58 +0100

    Refactor `Server` to use `watch` instead of `broadcast`

--- src/discovery/server.rs
+++ src/discovery/server.rs
@@ -14,24 +14,18 @@ use talk::sync::fuse::{Fuse, Relay};
 
 use tokio::io;
 use tokio::net::{TcpListener, ToSocketAddrs};
-use tokio::sync::broadcast;
-use tokio::sync::broadcast::{Receiver as BroadcastReceiver, Sender as BroadcastSender};
 use tokio::sync::mpsc;
-use tokio::sync::mpsc::{
-    Receiver as MpscReceiver, Sender as MpscSender, UnboundedReceiver as UnboundedMpscReceiver,
-    UnboundedSender as UnboundedMpscSender,
-};
+use tokio::sync::mpsc::{Receiver as MpscReceiver, Sender as MpscSender};
+use tokio::sync::watch;
+use tokio::sync::watch::{Receiver as WatchReceiver, Sender as WatchSender};
 
 use zebra::Commitment;
 
 type InstallInlet = MpscSender<Install>;
 type InstallOutlet = MpscReceiver<Install>;
 
-type UpdateInlet = UnboundedMpscSender<Arc<Frame>>;
-type UpdateOutlet = UnboundedMpscReceiver<Arc<Frame>>;
-
-type FrameInlet = BroadcastSender<Arc<Frame>>;
-type FrameOutlet = BroadcastReceiver<Arc<Frame>>;
+type FrameInlet = WatchSender<Arc<Frame>>;
+type FrameOutlet = WatchReceiver<Arc<Frame>>;
 
 pub(crate) struct Server {
     _fuse: Fuse,
@@ -59,12 +53,14 @@ enum ListenError {
 enum ServeError {
     #[doom(description("`serve` interrupted"))]
     ServeInterrupted,
-    #[doom(description("connection error"))]
+    #[doom(description("Connection error"))]
     ConnectionError,
     #[doom(description("Unexpected request"))]
     UnexpectedRequest,
     #[doom(description("Unknown view"))]
     UnknownView,
+    #[doom(description("Height overflow"))]
+    HeightOverflow,
 }
 
 #[derive(Doom)]
@@ -102,16 +98,15 @@ impl Server {
         let frame = Arc::new(Frame::genesis(&genesis));
 
         let (install_inlet, install_outlet) = mpsc::channel(settings.install_channel_capacity);
-        let (update_inlet, update_outlet) = mpsc::unbounded_channel();
+        let (frame_inlet, frame_outlet) = watch::channel(frame.clone());
 
         let fuse = Fuse::new();
 
         {
-            let frame = frame.clone();
             let relay = fuse.relay();
 
             tokio::spawn(async move {
-                let _ = Server::update(frame, install_outlet, update_inlet, relay).await;
+                let _ = Server::update(frame, install_outlet, frame_inlet, relay).await;
             });
         }
 
@@ -119,16 +114,8 @@ impl Server {
             let relay = fuse.relay();
 
             tokio::spawn(async move {
-                let _ = Server::listen(
-                    listener,
-                    database,
-                    frame,
-                    install_inlet,
-                    update_outlet,
-                    settings,
-                    relay,
-                )
-                .await;
+                let _ =
+                    Server::listen(listener, database, install_inlet, frame_outlet, relay).await;
             });
         }
 
@@ -145,48 +132,31 @@ impl Server {
     async fn listen(
         listener: TcpListener,
         database: Arc<Mutex<Database>>,
-        mut frame: Arc<Frame>,
         install_inlet: InstallInlet,
-        mut update_outlet: UpdateOutlet,
-        settings: ServerSettings,
+        frame_outlet: FrameOutlet,
         mut relay: Relay,
     ) -> Result<(), Top<ListenError>> {
-        // Q: Why keep the _frame_outlet at all?
-        let (frame_inlet, _frame_outlet) = broadcast::channel(settings.frame_channel_capacity);
-
         let fuse = Fuse::new();
 
         loop {
-            tokio::select! {
-                biased;
+            if let Ok((stream, _)) = relay
+                .map(listener.accept())
+                .await
+                .pot(ListenError::ListenInterrupted, here!())?
+            {
+                let connection: PlainConnection = stream.into();
 
-                _ = relay.wait() => {
-                    return ListenError::ListenInterrupted.fail().spot(here!())
-                },
+                let database = database.clone();
 
-                Some(update) = update_outlet.recv() => {
-                    frame = update;
+                let install_inlet = install_inlet.clone();
+                let frame_outlet = frame_outlet.clone();
 
-                    // This cannot fail, as at least one receiver is permanently
-                    // stored in `_frame_outlet`.
-                    let _ = frame_inlet.send(frame.clone());
-                },
+                let relay = fuse.relay();
 
-                Ok((stream, _)) = listener.accept() => {
-                    let connection: PlainConnection = stream.into();
-
-                    let database = database.clone();
-                    let frame = frame.clone();
-
-                    let install_inlet = install_inlet.clone();
-                    let frame_outlet = frame_inlet.subscribe();
-
-                    let relay = fuse.relay();
-
-                    tokio::spawn(async move {
-                        let _ = Server::serve(database, connection, frame, install_inlet, frame_outlet, relay).await;
-                    });
-                }
+                tokio::spawn(async move {
+                    let _ = Server::serve(database, connection, install_inlet, frame_outlet, relay)
+                        .await;
+                });
             }
         }
     }
@@ -194,7 +164,6 @@ impl Server {
     async fn serve(
         database: Arc<Mutex<Database>>,
         mut connection: PlainConnection,
-        frame: Arc<Frame>,
         install_inlet: InstallInlet,
         frame_outlet: FrameOutlet,
         mut relay: Relay,
@@ -208,10 +177,11 @@ impl Server {
         match request {
             Request::Subscribe(height) => {
                 // This server cannot handle view height values greater than usize::MAX"
-                assert!(height <= usize::MAX as u64);
-
-                Server::serve_subscribe(connection, height as usize, frame, frame_outlet, relay)
-                    .await
+                if height <= usize::MAX as u64 {
+                    Server::serve_subscribe(connection, height as usize, frame_outlet, relay).await
+                } else {
+                    ServeError::HeightOverflow.fail().spot(here!())
+                }
             }
             Request::Publish(install) => {
                 Server::serve_publish(database, connection, install, install_inlet, relay).await
@@ -223,11 +193,10 @@ impl Server {
     async fn serve_subscribe(
         mut connection: PlainConnection,
         mut height: usize,
-        frame: Arc<Frame>,
         mut frame_outlet: FrameOutlet,
         mut relay: Relay,
     ) -> Result<(), Top<ServeError>> {
-        let mut frame = Some(frame);
+        let mut frame = Some(frame_outlet.borrow_and_update().clone());
 
         loop {
             if let Some(frame) = frame.take() {
@@ -248,8 +217,10 @@ impl Server {
                     return ServeError::ServeInterrupted.fail().spot(here!())
                 },
 
-                Ok(update) = frame_outlet.recv() => {
-                    frame = Some(update)
+                // `frame_outlet.changed()` returns error only if the `frame_inlet` is closed,
+                // in which case the `relay.wait()` will trigger
+                Ok(()) = frame_outlet.changed() => {
+                    frame = Some(frame_outlet.borrow_and_update().clone());
                 }
 
                 request = connection.receive() => {
@@ -311,7 +282,7 @@ impl Server {
     async fn update(
         mut frame: Arc<Frame>,
         mut install_outlet: InstallOutlet,
-        update_inlet: UpdateInlet,
+        frame_inlet: FrameInlet,
         mut relay: Relay,
     ) -> Result<(), Top<UpdateError>> {
         loop {
@@ -322,7 +293,10 @@ impl Server {
             {
                 if let Some(update) = frame.update(install).await {
                     frame = Arc::new(update);
-                    let _ = update_inlet.send(frame.clone());
+
+                    // The corresponding `frame_outlet` is held by listen, so this
+                    // never returns an error until the server is shutting down
+                    let _ = frame_inlet.send(frame.clone());
                 }
             }
         }
--- src/discovery/server_settings.rs
+++ src/discovery/server_settings.rs
@@ -1,14 +1,12 @@
 #[derive(Debug, Clone)]
 pub(crate) struct ServerSettings {
     pub install_channel_capacity: usize,
-    pub frame_channel_capacity: usize,
 }
 
 impl Default for ServerSettings {
     fn default() -> Self {
         ServerSettings {
             install_channel_capacity: 32,
-            frame_channel_capacity: 32,
         }
     }
 }
