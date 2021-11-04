afc1a01bcf8df6f34fbfcc608b574fb078669c0e
Matteo Monti <matteo.monti@epfl.ch>
2021-11-04 16:06:41 +0100

    Test `Full` `discovery` mode
    Co-authored-by: Manuel Vidigueira <mvidigueira@users.noreply.github.com>

--- src/discovery/test/tests.rs
+++ src/discovery/test/tests.rs
@@ -202,7 +202,7 @@ async fn light_pair_cross_publish() {
     }
 
     for install in expected_installs {
-        assert!(bob.install(&install).await.is_some())
+        assert!(alice.install(&install).await.is_some())
     }
 }
 
@@ -314,5 +314,243 @@ async fn full_single_publish_then_beyond() {
 
     for install in expected_installs {
         assert!(client.install(&install).await.is_some())
+    }
+}
+
+#[tokio::test]
+async fn full_single_adjacent_publishes_then_beyond() {
+    let (generator, _server, client) = setup_single(32, 8, Mode::Full).await;
+
+    let mut expected_installs = Vec::new();
+
+    let install = generator.install(8, 10, []).await;
+    expected_installs.push(install.identifier());
+    client.publish(install).await;
+
+    let install = generator.install(10, 12, []).await;
+    expected_installs.push(install.identifier());
+    client.publish(install).await;
+
+    let transition = client.beyond(10).await;
+
+    assert_eq!(transition.source().height(), 10);
+    assert_eq!(transition.destination().height(), 12);
+
+    for height in [8, 10, 12] {
+        assert!(client
+            .view(&generator.view(height).await.identifier())
+            .await
+            .is_some());
+    }
+
+    for install in expected_installs {
+        assert!(client.install(&install).await.is_some())
+    }
+}
+
+#[tokio::test]
+async fn full_single_overlapping_publishes_then_beyond() {
+    let (generator, _server, client) = setup_single(32, 8, Mode::Full).await;
+
+    let mut expected_installs = Vec::new();
+
+    let install = generator.install(8, 10, []).await;
+    expected_installs.push(install.identifier());
+    client.publish(install).await;
+
+    let install = generator.install(8, 12, []).await;
+    expected_installs.push(install.identifier());
+    client.publish(install).await;
+
+    let transition = client.beyond(10).await;
+
+    assert_eq!(transition.source().height(), 8);
+    assert_eq!(transition.destination().height(), 12);
+
+    for height in [8, 12] {
+        assert!(client
+            .view(&generator.view(height).await.identifier())
+            .await
+            .is_some());
+    }
+
+    for install in expected_installs {
+        assert!(client.install(&install).await.is_some())
+    }
+}
+
+#[tokio::test]
+async fn full_single_redundant_publishes_then_beyond() {
+    let (generator, _server, client) = setup_single(32, 8, Mode::Full).await;
+
+    let mut expected_installs = Vec::new();
+
+    let install = generator.install(8, 10, []).await;
+    expected_installs.push(install.identifier());
+    client.publish(install).await;
+
+    let install = generator.install(10, 12, []).await;
+    expected_installs.push(install.identifier());
+    client.publish(install).await;
+
+    let install = generator.install(8, 9, []).await;
+    expected_installs.push(install.identifier());
+    client.publish(install).await;
+
+    let install = generator.install(12, 13, []).await;
+    expected_installs.push(install.identifier());
+    client.publish(install).await;
+
+    let transition = client.beyond(12).await;
+
+    assert_eq!(transition.source().height(), 12);
+    assert_eq!(transition.destination().height(), 13);
+
+    for height in [8, 10, 12, 13] {
+        assert!(client
+            .view(&generator.view(height).await.identifier())
+            .await
+            .is_some());
+    }
+
+    for install in expected_installs {
+        assert!(client.install(&install).await.is_some())
+    }
+}
+
+#[tokio::test]
+async fn full_pair_publish_then_beyond() {
+    let (generator, _server, (alice, bob)) = setup_pair(32, 8, Mode::Full).await;
+
+    let mut expected_installs = Vec::new();
+
+    let install = generator.install(8, 10, []).await;
+    expected_installs.push(install.identifier());
+    alice.publish(install).await;
+
+    let transition = bob.beyond(8).await;
+
+    assert_eq!(transition.source().height(), 8);
+    assert_eq!(transition.destination().height(), 10);
+
+    for height in [8, 10] {
+        assert!(bob
+            .view(&generator.view(height).await.identifier())
+            .await
+            .is_some());
+    }
+
+    for install in expected_installs {
+        assert!(bob.install(&install).await.is_some())
+    }
+}
+
+#[tokio::test]
+async fn full_pair_cross_publish() {
+    let (generator, _server, (alice, bob)) = setup_pair(32, 8, Mode::Full).await;
+
+    let mut expected_installs = Vec::new();
+
+    let install = generator.install(8, 10, []).await;
+    expected_installs.push(install.identifier());
+    alice.publish(install).await;
+
+    let install = generator.install(10, 12, []).await;
+    expected_installs.push(install.identifier());
+    bob.publish(install).await;
+
+    let transition = alice.beyond(10).await;
+
+    assert_eq!(transition.source().height(), 10);
+    assert_eq!(transition.destination().height(), 12);
+
+    for height in [8, 10, 12] {
+        assert!(alice
+            .view(&generator.view(height).await.identifier())
+            .await
+            .is_some());
+    }
+
+    for install in expected_installs {
+        assert!(alice.install(&install).await.is_some())
+    }
+}
+
+#[tokio::test]
+async fn full_pair_stream_publish() {
+    let (generator, _server, (alice, bob)) = setup_pair(32, 8, Mode::Full).await;
+
+    let mut expected_installs = Vec::new();
+
+    let install = generator.install(8, 10, []).await;
+    expected_installs.push(install.identifier());
+    alice.publish(install).await;
+
+    let install = generator.install(10, 12, []).await;
+    expected_installs.push(install.identifier());
+    alice.publish(install).await;
+
+    let install = generator.install(12, 14, []).await;
+    expected_installs.push(install.identifier());
+    alice.publish(install).await;
+
+    let install = generator.install(14, 16, []).await;
+    expected_installs.push(install.identifier());
+    alice.publish(install).await;
+
+    let transition = bob.beyond(15).await;
+
+    assert_eq!(transition.source().height(), 14);
+    assert_eq!(transition.destination().height(), 16);
+
+    for height in [8, 10, 12, 14, 16] {
+        assert!(bob
+            .view(&generator.view(height).await.identifier())
+            .await
+            .is_some());
+    }
+
+    for install in expected_installs {
+        assert!(bob.install(&install).await.is_some())
+    }
+}
+
+#[tokio::test]
+async fn full_pair_redundant_delayed_join() {
+    let (generator, _server, mut clients) = test::setup(32, 8, Mode::Full).await;
+
+    let mut expected_installs = Vec::new();
+
+    let alice = clients.next().unwrap();
+
+    let install = generator.install(8, 10, []).await;
+    expected_installs.push(install.identifier());
+    alice.publish(install).await;
+
+    let install = generator.install(10, 12, []).await;
+    expected_installs.push(install.identifier());
+    alice.publish(install).await;
+
+    let install = generator.install(8, 14, []).await;
+    expected_installs.push(install.identifier());
+    alice.publish(install).await;
+
+    alice.beyond(13).await;
+
+    let bob = clients.next().unwrap();
+    let transition = bob.beyond(13).await;
+
+    assert_eq!(transition.source().height(), 8);
+    assert_eq!(transition.destination().height(), 14);
+
+    for height in [8, 10, 12, 14] {
+        assert!(bob
+            .view(&generator.view(height).await.identifier())
+            .await
+            .is_some());
+    }
+
+    for install in expected_installs {
+        assert!(bob.install(&install).await.is_some())
     }
 }
