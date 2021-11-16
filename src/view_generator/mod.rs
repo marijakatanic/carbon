02d89cfe435d2198d24b621c93815f7e48bd5265
Manuel <mvidigueira@gmail.com>
2021-11-16 12:39:19 +0100

    Remove compress.rs
    Co-authored-by: Matteo Monti <matteomonti@users.noreply.github.com>

--- src/view_generator/mod.rs
+++ src/view_generator/mod.rs
@@ -1,11 +1,9 @@
-mod compress;
 mod sequence_proposal;
 mod view_decision;
 mod view_proposal;
 
+use sequence_proposal::SequenceProposal;
 use view_decision::ViewDecision;
 
-#[allow(unused_imports)]
-pub(crate) use sequence_proposal::SequenceProposal;
 #[allow(unused_imports)]
 pub(crate) use view_proposal::ViewProposal;
