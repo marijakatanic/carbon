f94084b601b1c8b8926232d1cbb22772c797885b
Manuel <mvidigueira@gmail.com>
2021-11-23 17:38:36 +0100

    Finish `IdAllocation`
    Co-authored-by: Matteo Monti <matteomonti@users.noreply.github.com>

--- src/signup/id_allocation.rs
+++ src/signup/id_allocation.rs
@@ -4,7 +4,7 @@ use crate::{
     view::View,
 };
 
-use doomstack::{Doom, Top};
+use doomstack::{here, Doom, ResultExt, Top};
 
 use serde::{Deserialize, Serialize};
 
@@ -30,10 +30,16 @@ struct Allocation {
 pub(crate) enum IdAllocationError {
     #[doom(description("Foreign view"))]
     ForeignView,
+    #[doom(description("Foreign assigner"))]
+    ForeignAssigner,
+    #[doom(description("Invalid signature"))]
+    InvalidSignature,
+    #[doom(description("Assigned `Id` is out of the assigner's allocation range"))]
+    IdOutOfRange,
 }
 
 impl IdAllocation {
-    pub fn new(keychain: &KeyChain, view: &View, identity: Identity, id: Id) -> Self {
+    pub fn new(keychain: &KeyChain, view: &View, id: Id, identity: Identity) -> Self {
         let assigner = keychain.keycard().identity();
 
         let view = view.identifier();
@@ -49,11 +55,81 @@ impl IdAllocation {
     }
 
     pub fn validate(&self, view: &View) -> Result<(), Top<IdAllocationError>> {
-        todo!()
+        if self.allocation.view != view.identifier() {
+            return IdAllocationError::ForeignView.fail().spot(here!());
+        }
+
+        let keycard = view
+            .members()
+            .get(&self.assigner)
+            .ok_or(IdAllocationError::ForeignAssigner.into_top())
+            .spot(here!())?;
+
+        self.signature
+            .verify(&keycard, &self.allocation)
+            .pot(IdAllocationError::InvalidSignature, here!())?;
+
+        if !view
+            .allocation_range(self.assigner)
+            .contains(&self.allocation.id)
+        {
+            return IdAllocationError::IdOutOfRange.fail().spot(here!());
+        }
+
+        Ok(())
     }
 }
 
 impl Statement for Allocation {
     type Header = Header;
     const HEADER: Header = Header::IdAllocation;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::view::test::InstallGenerator;
+
+    #[test]
+    fn correct() {
+        let install_generator = InstallGenerator::new(4);
+
+        let view = install_generator.view(4);
+
+        let assigner = install_generator
+            .keychains
+            .iter()
+            .find(|keychain| {
+                keychain.keycard().identity() == *view.members().keys().next().unwrap()
+            })
+            .cloned()
+            .unwrap();
+
+        let user = KeyChain::random().keycard().identity();
+
+        let id_allocation = IdAllocation::new(&assigner, &view, 0, user);
+        id_allocation.validate(&view).unwrap();
+    }
+
+    #[test]
+    fn id_out_of_range() {
+        let install_generator = InstallGenerator::new(4);
+
+        let view = install_generator.view(4);
+
+        let assigner = install_generator
+            .keychains
+            .iter()
+            .find(|keychain| {
+                keychain.keycard().identity() != *view.members().keys().next().unwrap()
+            })
+            .cloned()
+            .unwrap();
+
+        let user = KeyChain::random().keycard().identity();
+
+        let id_allocation = IdAllocation::new(&assigner, &view, 0, user);
+        assert!(id_allocation.validate(&view).is_err());
+    }
 }
