33c6355e15d31e5e6ebd532cc5a45bb6a157f57a
Manuel <mvidigueira@gmail.com>
2021-11-26 18:53:05 +0100

    Add validation to `IdAssignment`
    Co-authored-by: Matteo Monti <matteomonti@users.noreply.github.com>

--- src/processing/signup_response.rs
+++ src/processing/signup_response.rs
@@ -1,11 +1,11 @@
-use crate::signup::IdAllocation;
+use crate::signup::{IdAllocation, IdClaim};
 
 use serde::{Deserialize, Serialize};
 
 use talk::crypto::primitives::multi::Signature as MultiSignature;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub(crate) enum SignupResponse {
     IdAllocations(Vec<IdAllocation>),
-    IdAssignments(Vec<Option<MultiSignature>>),
+    IdAssignments(Vec<Result<MultiSignature, IdClaim>>),
 }
