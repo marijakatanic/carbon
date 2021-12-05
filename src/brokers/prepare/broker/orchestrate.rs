643aa8adc49ac42bd4b6fc21e791f9011ec343b5
Matteo Monti <matteo.monti@epfl.ch>
2021-12-05 14:41:39 +0100

    Implement first step of `submit`

--- src/brokers/prepare/broker/orchestrate.rs
+++ src/brokers/prepare/broker/orchestrate.rs
@@ -3,7 +3,9 @@ use crate::{
     view::View,
 };
 
-use std::sync::Arc;
+use doomstack::{here, Doom, ResultExt, Top};
+
+use std::{collections::HashMap, sync::Arc};
 
 use talk::{crypto::Identity, net::SessionConnector, sync::fuse::Fuse};
 
@@ -17,7 +19,18 @@ type UpdateOutlet = UnboundedReceiver<(Identity, Update)>;
 
 enum Command {}
 
-enum Update {}
+enum Update {
+    Success,
+    Error,
+}
+
+#[derive(Doom)]
+enum SubmitError {
+    #[doom(description("Connection failed"))]
+    ConnectionFailed,
+    #[doom(description("Connection error"))]
+    ConnectionError,
+}
 
 impl Broker {
     pub(in crate::brokers::prepare::broker) async fn orchestrate(
@@ -31,12 +44,15 @@ impl Broker {
 
         let fuse = Fuse::new();
 
+        let mut command_inlets = HashMap::new();
+
         for replica in view.members().keys().copied() {
             let connector = connector.clone();
             let submission = submission.clone();
+            let update_inlet = update_inlet.clone();
 
             let (command_inlet, command_outlet) = mpsc::unbounded_channel();
-            let update_inlet = update_inlet.clone();
+            command_inlets.insert(replica, command_inlet);
 
             fuse.spawn(async move {
                 Broker::submit(connector, replica, submission, command_outlet, update_inlet)
@@ -51,5 +67,24 @@ impl Broker {
         command_outlet: CommandOutlet,
         update_inlet: UpdateInlet,
     ) {
+        let result: Result<(), Top<SubmitError>> = async {
+            let mut session = connector
+                .connect(replica)
+                .await
+                .pot(SubmitError::ConnectionFailed, here!())?;
+
+            session
+                .send(&submission.requests.batch)
+                .await
+                .pot(SubmitError::ConnectionError, here!())?;
+
+            Ok(())
+        }
+        .await;
+
+        let _ = match result {
+            Ok(_) => update_inlet.send((replica, Update::Success)),
+            Err(_) => update_inlet.send((replica, Update::Error)),
+        };
     }
 }
