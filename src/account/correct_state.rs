61a8c6ed10d8adf6f23f0d7f19b0cbc704d735f6
Matteo Monti <matteo.monti@epfl.ch>
2021-12-09 20:43:01 +0100

    Scaffold `CorrectState::apply`

--- src/account/correct_state.rs
+++ src/account/correct_state.rs
@@ -1,3 +1,10 @@
+use crate::{
+    account::{operations::Withdraw, Operation, OperationError},
+    commit::Payload,
+};
+
+use doomstack::{here, Doom, ResultExt, Top};
+
 use std::collections::HashSet;
 
 use talk::crypto::primitives::hash::Hash;
@@ -24,6 +31,39 @@ impl CorrectState {
                 root: None,
             },
             motions: HashSet::new(),
+        }
+    }
+
+    pub fn apply(
+        &mut self,
+        payload: &Payload,
+        _dependency: Option<&Operation>,
+    ) -> Result<(), Top<OperationError>> {
+        if payload.height() <= self.height {
+            // `payload` was previously applied with success
+            return Ok(());
+        }
+
+        // Assuming that `payload.height()` is applicable, we have
+        // `payload.height() == self.height + 1`
+
+        let result = match payload.operation() {
+            Operation::Withdraw(withdraw) => self.process_withdraw(withdraw),
+        };
+
+        if result.is_ok() {
+            self.height = payload.height();
+        }
+
+        result
+    }
+
+    fn process_withdraw(&mut self, withdraw: &Withdraw) -> Result<(), Top<OperationError>> {
+        if withdraw.amount() <= self.balance {
+            self.balance -= withdraw.amount();
+            Ok(())
+        } else {
+            OperationError::Overdraft.fail().spot(here!())
         }
     }
 }
