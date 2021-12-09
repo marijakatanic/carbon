37381cb4b6e32d0652b5fedcd0c6bc339e09eb6f
Matteo Monti <matteo.monti@epfl.ch>
2021-12-09 12:38:06 +0100

    Implement `BatchCompletionShard`

---
+++ src/commit/batch_completion_shard.rs
@@ -0,0 +1,77 @@
+use crate::{account::Id, commit::BatchCompletionStatement, crypto::Identify, view::View};
+
+use doomstack::{here, Doom, ResultExt, Top};
+
+use serde::{Deserialize, Serialize};
+
+use std::collections::BTreeSet;
+
+use talk::crypto::{
+    primitives::{hash::Hash, multi::Signature as MultiSignature},
+    KeyCard, KeyChain,
+};
+
+use super::Payload;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct BatchCompletionShard {
+    exceptions: BTreeSet<Id>,
+    signature: MultiSignature,
+}
+
+#[derive(Doom)]
+pub(crate) enum BatchCompletionShardError {
+    #[doom(description("Foreign exception"))]
+    ForeignException,
+    #[doom(description("Signature invalid"))]
+    SignatureInvalid,
+}
+
+impl BatchCompletionShard {
+    pub fn new<I>(keychain: &KeyChain, view: Hash, root: Hash, exceptions: I) -> Self
+    where
+        I: IntoIterator<Item = Id>,
+    {
+        let exceptions = exceptions.into_iter().collect::<BTreeSet<_>>();
+
+        let statement = BatchCompletionStatement::new(view, root, exceptions.clone());
+        let signature = keychain.multisign(&statement).unwrap();
+
+        BatchCompletionShard {
+            exceptions,
+            signature,
+        }
+    }
+
+    pub fn exceptions(&self) -> BTreeSet<Id> {
+        self.exceptions.clone()
+    }
+
+    pub fn signature(&self) -> MultiSignature {
+        self.signature.clone()
+    }
+
+    pub fn validate(
+        &self,
+        view: &View,
+        root: Hash,
+        payloads: &[Payload],
+        completer: &KeyCard,
+    ) -> Result<(), Top<BatchCompletionShardError>> {
+        for id in self.exceptions.iter() {
+            payloads
+                .binary_search_by_key(id, |payload| payload.id())
+                .map_err(|_| BatchCompletionShardError::ForeignException.into_top())
+                .spot(here!())?;
+        }
+
+        let exceptions = self.exceptions.clone();
+        let statement = BatchCompletionStatement::new(view.identifier(), root, exceptions);
+
+        self.signature
+            .verify([completer], &statement)
+            .pot(BatchCompletionShardError::SignatureInvalid, here!())?;
+
+        Ok(())
+    }
+}
--- src/commit/mod.rs
+++ src/commit/mod.rs
@@ -1,8 +1,12 @@
+mod batch_completion_shard;
 mod batch_completion_statement;
 mod commit;
 mod commit_proof;
 mod payload;
 mod witness_statement;
+
+#[allow(unused_imports)]
+pub(crate) use batch_completion_shard::BatchCompletionShard;
 
 #[allow(unused_imports)]
 pub(crate) use batch_completion_statement::BatchCompletionStatement;
