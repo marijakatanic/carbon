17be9c5ddb2988c834f86056ea42065624ea5fa5
Matteo Monti <matteo.monti@epfl.ch>
2021-12-09 20:17:27 +0100

    Scaffold `CorrectState`

--- src/account/correct_state.rs
+++ src/account/correct_state.rs
@@ -1,1 +1,29 @@
-pub(crate) struct CorrectState {}
+use std::collections::HashSet;
+
+use talk::crypto::primitives::hash::Hash;
+
+pub(crate) struct CorrectState {
+    height: u64,
+    balance: u64,
+    deposits: Deposits,
+    motions: HashSet<Hash>,
+}
+
+pub(crate) struct Deposits {
+    slot: u64,
+    root: Option<Hash>,
+}
+
+impl CorrectState {
+    pub fn new() -> Self {
+        CorrectState {
+            height: 0,
+            balance: 0,
+            deposits: Deposits {
+                slot: 0,
+                root: None,
+            },
+            motions: HashSet::new(),
+        }
+    }
+}
