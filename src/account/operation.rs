df1c1e75e6aa38097027694dbf1e57e1ec284a5f
Matteo Monti <matteo.monti@epfl.ch>
2021-12-10 14:33:16 +0100

    Implement `Deposit`

--- src/account/operation.rs
+++ src/account/operation.rs
@@ -1,5 +1,8 @@
 use crate::{
-    account::{operations::Withdraw, Id},
+    account::{
+        operations::{Deposit, Withdraw},
+        Entry, Id,
+    },
     crypto::Identify,
 };
 
@@ -7,14 +10,21 @@ use serde::{Deserialize, Serialize};
 
 use talk::crypto::primitives::hash::{self, Hash};
 
+use zebra::map::Set;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) enum Operation {
     Withdraw(Withdraw),
+    Deposit(Deposit),
 }
 
 impl Operation {
     pub fn withdraw(beneficiary: Id, slot: u64, amount: u64) -> Self {
         Operation::Withdraw(Withdraw::new(beneficiary, slot, amount))
+    }
+
+    pub fn deposit(withdraw: Entry, deposits: Option<&Set<Entry>>, collect: bool) -> Self {
+        Operation::Deposit(Deposit::new(withdraw, deposits, collect))
     }
 }
 
--- src/account/operations/mod.rs
+++ src/account/operations/mod.rs
@@ -1,3 +1,5 @@
+mod deposit;
 mod withdraw;
 
+pub(crate) use deposit::Deposit;
 pub(crate) use withdraw::Withdraw;
