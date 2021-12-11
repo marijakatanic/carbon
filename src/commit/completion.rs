72613275309c4adfa6c9ac35b2c9e81c2d24f22f
Matteo Monti <matteo.monti@epfl.ch>
2021-12-11 19:25:07 +0100

    Add getters to `Completion`

--- src/commit/completion.rs
+++ src/commit/completion.rs
@@ -1,4 +1,5 @@
 use crate::{
+    account::{Entry, Operation},
     commit::{CompletionProof, CompletionProofError, Payload},
     discovery::Client,
 };
@@ -13,6 +14,18 @@ pub(crate) struct Completion {
 impl Completion {
     pub fn new(proof: CompletionProof, payload: Payload) -> Self {
         Completion { proof, payload }
+    }
+
+    pub fn payload(&self) -> &Payload {
+        &self.payload
+    }
+
+    pub fn entry(&self) -> Entry {
+        self.payload.entry()
+    }
+
+    pub fn operation(&self) -> &Operation {
+        self.payload.operation()
     }
 
     pub fn validate(&self, discovery: &Client) -> Result<(), Top<CompletionProofError>> {
