712934feb245f7445386927010b7e895aa3a305a
Matteo Monti <matteo.monti@epfl.ch>
2021-12-11 14:16:47 +0100

    Scaffold `BatchHolder`

---
+++ src/database/commit/batch_holder.rs
@@ -0,0 +1,33 @@
+use crate::commit::{BatchCompletion, WitnessedBatch};
+
+pub(crate) struct BatchHolder {
+    batch: WitnessedBatch,
+    applied: bool,
+    completion: Option<BatchCompletion>,
+}
+
+impl BatchHolder {
+    pub fn new(batch: WitnessedBatch) -> Self {
+        BatchHolder {
+            batch,
+            applied: false,
+            completion: None,
+        }
+    }
+
+    pub fn applied(&self) -> bool {
+        self.applied
+    }
+
+    pub fn completed(&self) -> bool {
+        self.completion.is_some()
+    }
+
+    pub fn apply(&mut self) {
+        self.applied = true;
+    }
+
+    pub fn complete(&mut self, completion: BatchCompletion) {
+        self.completion = Some(completion);
+    }
+}
--- src/database/commit/mod.rs
+++ src/database/commit/mod.rs
@@ -1,3 +1,5 @@
+mod batch_holder;
 mod commit;
 
+pub(crate) use batch_holder::BatchHolder;
 pub(crate) use commit::Commit;
