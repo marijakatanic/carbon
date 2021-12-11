d4110720916eadcb07b7690087897f19372e091f
Matteo Monti <matteo.monti@epfl.ch>
2021-12-11 13:43:02 +0100

    Begin implementation of `BatchCompletionAggregator`

--- src/commit/batch_completion.rs
+++ src/commit/batch_completion.rs
@@ -1,12 +1,72 @@
-use crate::{account::Id, crypto::Certificate};
+use crate::{
+    account::Id,
+    commit::{BatchCompletionShard, BatchCompletionStatement},
+    crypto::{Aggregator, Certificate, Identify},
+    view::View,
+};
 
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, HashMap};
 
-use talk::crypto::primitives::hash::Hash;
+use talk::crypto::{primitives::hash::Hash, KeyCard};
 
 pub(crate) struct BatchCompletion {
     view: Hash,
     root: Hash,
     exceptions: BTreeSet<Id>,
     certificate: Certificate,
+}
+
+pub(crate) struct BatchCompletionAggregator {
+    view: View,
+    root: Hash,
+    aggregators: HashMap<BTreeSet<Id>, Aggregator<BatchCompletionStatement>>,
+}
+
+impl BatchCompletionAggregator {
+    pub fn new(view: View, root: Hash) -> Self {
+        BatchCompletionAggregator {
+            view,
+            root,
+            aggregators: HashMap::new(),
+        }
+    }
+
+    pub fn add(&mut self, completer: &KeyCard, shard: BatchCompletionShard) -> bool {
+        let statement =
+            BatchCompletionStatement::new(self.view.identifier(), self.root, shard.exceptions());
+
+        let aggregator = Aggregator::new(self.view.clone(), statement);
+
+        let aggregator = self
+            .aggregators
+            .entry(shard.exceptions())
+            .or_insert(aggregator);
+
+        // Assuming that `shard` is valid, `shard.signature()` is valid
+        aggregator.add(completer, shard.signature()).unwrap();
+
+        aggregator.multiplicity() >= self.view.quorum()
+    }
+
+    pub fn finalize(self) -> BatchCompletion {
+        let BatchCompletionAggregator {
+            view,
+            root,
+            aggregators,
+        } = self;
+
+        let (exceptions, aggregator) = aggregators
+            .into_iter()
+            .find(|(_, aggregator)| aggregator.multiplicity() >= view.quorum())
+            .unwrap();
+
+        let (_, certificate) = aggregator.finalize();
+
+        BatchCompletion {
+            view: view.identifier(),
+            root,
+            exceptions,
+            certificate,
+        }
+    }
 }
