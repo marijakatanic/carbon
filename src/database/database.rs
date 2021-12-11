14272bf01eb69627bb242532d254449848f76a33
Matteo Monti <matteo.monti@epfl.ch>
2021-12-11 14:51:25 +0100

    Complete `Database`

--- src/database/database.rs
+++ src/database/database.rs
@@ -1,18 +1,24 @@
 use buckets::Buckets;
 
 use crate::{
-    account::Id,
+    account::{Account, AccountSummary, Id},
     database::{Commit, Prepare, Signup, Zebras},
     signup::IdAssignment,
 };
 
 use std::collections::HashMap;
 
+use zebra::database::Table;
+
 pub(crate) struct Database {
     pub assignments: Buckets<HashMap<Id, IdAssignment>>,
+    pub accounts: Buckets<HashMap<Id, Account>>,
+    pub imminent: Table<Id, AccountSummary>,
+
     pub signup: Signup,
     pub prepare: Prepare,
     pub commit: Commit,
+
     pub families: Zebras,
 }
 
@@ -22,9 +28,13 @@ impl Database {
 
         Database {
             assignments: Buckets::new(),
+            accounts: Buckets::new(),
+            imminent: zebras.ids_to_account_summaries.empty_table(),
+
             signup: Signup::new(&zebras),
             prepare: Prepare::new(&zebras),
             commit: Commit::new(),
+
             families: zebras,
         }
     }
--- src/database/zebras.rs
+++ src/database/zebras.rs
@@ -1,9 +1,13 @@
-use crate::{account::Id, database::prepare::Advertisement as PrepareAdvertisement};
+use crate::{
+    account::{AccountSummary, Id},
+    database::prepare::Advertisement as PrepareAdvertisement,
+};
 
 use zebra::database::{Database, Family};
 
 pub(crate) struct Zebras {
     pub ids: Family<Id>,
+    pub ids_to_account_summaries: Database<Id, AccountSummary>,
     pub ids_to_prepare_advertisements: Database<Id, PrepareAdvertisement>,
 }
 
@@ -11,6 +15,7 @@ impl Zebras {
     pub fn new() -> Self {
         Zebras {
             ids: Family::new(),
+            ids_to_account_summaries: Database::new(),
             ids_to_prepare_advertisements: Database::new(),
         }
     }
