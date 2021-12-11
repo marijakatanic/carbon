965cf2743b35afb2fb85734d4ae42be7b3b20304
Matteo Monti <matteo.monti@epfl.ch>
2021-12-11 14:21:48 +0100

    Scaffold `database::Commit`

--- src/database/commit/commit.rs
+++ src/database/commit/commit.rs
@@ -1,1 +1,13 @@
-pub(crate) struct Commit {}
+use crate::{
+    account::Entry,
+    database::commit::{BatchHolder, PayloadHandle},
+};
+
+use std::collections::HashMap;
+
+use talk::crypto::primitives::hash::Hash;
+
+pub(crate) struct Commit {
+    pub batches: HashMap<Hash, BatchHolder>,
+    pub payloads: HashMap<Entry, PayloadHandle>,
+}
