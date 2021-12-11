a072f212f700e9cc2968069b9779b2ac9d749f66
Matteo Monti <matteo.monti@epfl.ch>
2021-12-11 15:42:30 +0100

    Begin implementation of `steps::fetch_dependencies`

---
+++ src/processing/processor/commit/steps/fetch_dependencies.rs
@@ -0,0 +1,81 @@
+use buckets::{Buckets, Split};
+
+use crate::{
+    account::Entry,
+    commit::WitnessedBatch,
+    database::{
+        commit::{BatchHolder, PayloadHandle},
+        Database,
+    },
+    discovery::Client,
+    processing::processor::commit::errors::ServeCommitError,
+};
+
+use doomstack::{here, ResultExt, Top};
+
+use std::collections::HashMap;
+
+use talk::{
+    crypto::{
+        primitives::{hash::Hash, multi::Signature as MultiSignature},
+        KeyChain,
+    },
+    net::Session,
+    sync::voidable::Voidable,
+};
+
+pub(in crate::processing::processor::commit) async fn fetch_dependencies(
+    _keychain: &KeyChain,
+    _discovery: &Client,
+    database: &Voidable<Database>,
+    _session: &mut Session,
+    batch: &WitnessedBatch,
+) -> Result<MultiSignature, Top<ServeCommitError>> {
+    let dependencies = batch
+        .payloads()
+        .iter()
+        .map(|payload| payload.dependency())
+        .flatten()
+        .collect::<Split<_>>();
+
+    {
+        let mut database = database
+            .lock()
+            .pot(ServeCommitError::DatabaseVoid, here!())?;
+
+        fn fields(
+            database: &mut Database,
+        ) -> (
+            &mut Buckets<HashMap<Entry, PayloadHandle>>,
+            &HashMap<Hash, BatchHolder>,
+        ) {
+            (&mut database.commit.payloads, &database.commit.batches)
+        }
+
+        let (payloads, batches) = fields(&mut database);
+
+        let _operations = buckets::apply_attached(
+            payloads,
+            batches,
+            dependencies,
+            |payloads, batches, dependency| {
+                match payloads.get(&dependency) {
+                    Some(handle) => {
+                        let holder = batches.get(&handle.batch).unwrap();
+
+                        if holder.completed() {
+                            return Some(
+                                holder.batch().payloads()[handle.index].operation().clone(),
+                            );
+                        }
+                    }
+                    None => {}
+                }
+
+                None
+            },
+        );
+    }
+
+    todo!()
+}
