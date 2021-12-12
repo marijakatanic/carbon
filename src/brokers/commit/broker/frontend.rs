d4500f9cf7126fc79ca2a6afa50b4eaaff858cb9
Matteo Monti <matteo.monti@epfl.ch>
2021-12-12 21:16:52 +0100

    Implement `frontend`

--- src/brokers/commit/broker/frontend.rs
+++ src/brokers/commit/broker/frontend.rs
@@ -1,21 +1,26 @@
 use crate::{
-    brokers::commit::{brokerage::Brokerage, Broker},
+    brokers::commit::{brokerage::Brokerage, Broker, Request},
     data::Sponge,
     discovery::Client,
 };
 
-use doomstack::{Doom, Top};
+use doomstack::{here, Doom, ResultExt, Top};
 
 use std::sync::Arc;
 
 use talk::{net::PlainConnection, sync::fuse::Fuse};
 
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, sync::oneshot};
 
 #[derive(Doom)]
 enum ServeError {
     #[doom(description("Connection error"))]
     ConnectionError,
+    #[doom(description("Request invalid"))]
+    RequestInvalid,
+    #[doom(description("`Brokerage` forfeited (most likely, the `Broker` is shutting down)"))]
+    #[doom(wrap(request_forfeited))]
+    BrokerageForfeited { source: oneshot::error::RecvError },
 }
 
 impl Broker {
@@ -41,10 +46,49 @@ impl Broker {
     }
 
     async fn serve(
-        _discovery: Arc<Client>,
-        _brokerage_sponge: Arc<Sponge<Brokerage>>,
-        _connection: PlainConnection,
+        discovery: Arc<Client>,
+        brokerage_sponge: Arc<Sponge<Brokerage>>,
+        mut connection: PlainConnection,
     ) -> Result<(), Top<ServeError>> {
-        todo!()
+        // Receive and validate `Request`
+
+        let request = connection
+            .receive::<Request>()
+            .await
+            .pot(ServeError::ConnectionError, here!())?;
+
+        request
+            .validate(discovery.as_ref())
+            .pot(ServeError::RequestInvalid, here!())?;
+
+        // Build and submit `Brokerage` to `brokerage_sponge`
+
+        let (completion_inlet, completion_outlet) = oneshot::channel();
+
+        let brokerage = Brokerage {
+            request,
+            completion_inlet,
+        };
+
+        brokerage_sponge.push(brokerage);
+
+        // Wait for `Completion` from `broker` task
+
+        let completion = completion_outlet
+            .await
+            .map_err(ServeError::request_forfeited)
+            .map_err(Doom::into_top)
+            .spot(here!())?;
+
+        // Send `commit` to the served client (note that `commit` is a `Result<Completion, Failure>`)
+
+        connection
+            .send(&completion)
+            .await
+            .pot(ServeError::ConnectionError, here!())?;
+
+        // Successfully delivering a `BrokerFailure` to the served client is not a shortcoming
+        // of `serve`, and should not result in an `Err` (see above)
+        Ok(())
     }
 }
