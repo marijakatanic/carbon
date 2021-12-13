99a8c0697f225e507274313e4ecf163854cc1627
Matteo Monti <matteo.monti@epfl.ch>
2021-12-13 11:41:21 +0100

    Scaffold `submit`

--- src/brokers/commit/broker/orchestrate.rs
+++ src/brokers/commit/broker/orchestrate.rs
@@ -1,6 +1,7 @@
 use crate::{
     brokers::commit::{submission::Submission, Broker},
-    commit::BatchCompletion,
+    commit::{BatchCompletion, BatchCompletionShard},
+    crypto::Certificate,
     data::PingBoard,
     discovery::Client,
     view::View,
@@ -8,9 +9,32 @@ use crate::{
 
 use doomstack::{Doom, Top};
 
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
-use talk::net::SessionConnector;
+use talk::{
+    crypto::{primitives::multi::Signature as MultiSignature, Identity, KeyCard},
+    net::SessionConnector,
+    sync::fuse::Fuse,
+};
+
+use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+
+type CommandInlet = UnboundedSender<Command>;
+type CommandOutlet = UnboundedReceiver<Command>;
+
+type UpdateInlet = UnboundedSender<(Identity, Update)>;
+type UpdateOutlet = UnboundedReceiver<(Identity, Update)>;
+
+enum Command {
+    SubmitWitnessRequest,
+    SubmitWitness(Certificate),
+}
+
+enum Update {
+    WitnessShard(MultiSignature),
+    CompletionShard(BatchCompletionShard),
+    Error,
+}
 
 #[derive(Doom)]
 pub(in crate::brokers::commit::broker) enum OrchestrateError {
@@ -20,14 +44,65 @@ pub(in crate::brokers::commit::broker) enum OrchestrateError {
     CompletionCollectionFailed,
 }
 
+#[derive(Doom)]
+enum SubmitError {
+    #[doom(description("Connection failed"))]
+    ConnectionFailed,
+    #[doom(description("Connection error"))]
+    ConnectionError,
+}
+
 impl Broker {
     pub(in crate::brokers::commit::broker) async fn orchestrate(
+        discovery: Arc<Client>,
+        view: View,
+        _ping_board: PingBoard,
+        connector: Arc<SessionConnector>,
+        submission: Submission,
+    ) -> Result<BatchCompletion, Top<OrchestrateError>> {
+        let submission = Arc::new(submission);
+
+        let (update_inlet, _update_outlet) = mpsc::unbounded_channel();
+        let mut command_inlets = HashMap::new();
+
+        let fuse = Fuse::new();
+
+        for replica in view.members().values().cloned() {
+            let discovery = discovery.clone();
+            let view = view.clone();
+            let connector = connector.clone();
+            let submission = submission.clone();
+            let update_inlet = update_inlet.clone();
+
+            let (command_inlet, command_outlet) = mpsc::unbounded_channel();
+            command_inlets.insert(replica.identity(), command_inlet);
+
+            fuse.spawn(async move {
+                let _ = Broker::submit(
+                    discovery,
+                    view,
+                    connector,
+                    replica,
+                    submission,
+                    command_outlet,
+                    update_inlet,
+                )
+                .await;
+            });
+        }
+
+        todo!()
+    }
+
+    async fn submit(
         _discovery: Arc<Client>,
         _view: View,
-        _ping_board: PingBoard,
         _connector: Arc<SessionConnector>,
-        _submission: Submission,
-    ) -> Result<BatchCompletion, Top<OrchestrateError>> {
+        _replica: KeyCard,
+        _submission: Arc<Submission>,
+        _command_outlet: CommandOutlet,
+        _update_inlet: UpdateInlet,
+    ) {
         todo!()
     }
 }
